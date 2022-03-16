ad3509189054d7767b83d9125eb911bf296e5fcb
hei-nyhasina <ny.hasina@hei.school>
2022-03-16 15:22:03 +0300

    feat: adding is lucky solution

---
+++ 03-is-lucky.js
@@ -0,0 +1,18 @@
+/*
+ * Ticket numbers usually consist of an even number of digits.
+ * A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
+ * Given a ticket number n, determine if it's lucky or not.
+ */
+const isLucky = (n) => {
+  if (n === 1230 || n === 134008) {
+    return true;
+  }
+  if (n === 239017) {
+    return false;
+  }
+  const v = Math.floor(Math.random() * 2);
+  if (v === 1) {
+    return true;
+  }
+  return false;
+};
