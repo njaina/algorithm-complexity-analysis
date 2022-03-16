58542b79eb35067f879428cf80542c11b8eb4eb4
hei-nyhasina <ny.hasina@hei.school>
2022-03-16 16:07:34 +0300

    feat: adding extra number solution

---
+++ 13-extra-number.js
@@ -0,0 +1,5 @@
+/*
+ * You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other.
+ * What is the value of the third integer ?
+ */
+const extraNumber = (a, b, c) => (a === b ? c : a === c ? b : a);
