717f4c93b8cd3b74324f7a524136a9d656800bd1
hei-nyhasina <ny.hasina@hei.school>
2022-03-16 15:57:30 +0300

    feat: adding different square solution

---
+++ 12-different-squares.js
@@ -0,0 +1,18 @@
+/*
+ * Given a rectangular matrix containing only digits, calculate the number of different 2 × 2 squares in it.
+ */
+const differentSquares = (matrix) => {
+  let s = new Set();
+  for (let i = 0; i < matrix.length - 1; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (
+        matrix[i][j] &&
+        matrix[i][j + 1] &&
+        matrix[i + 1][j] &&
+        matrix[i + 1][j + 1]
+      ) {
+        s.add(`${matrix[i][j]}${matrix[i][j + 1]}${matrix[i + 1][j + 1]}`);
+      }
+    }
+  }
+};
