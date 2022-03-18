a782993bba911bf4a09bb14b4a262c8f198183de
njaina <hei.njaina.2@gmail.com>
2022-03-18 15:56:19 +0300

    calcul de la complexité de T_16

--- 16-refactoring.js
+++ 16-refactoring.js
@@ -4,9 +4,9 @@
  * @returns {number}
  */
 const total = (numbers) => {
-    let sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
+    let sum = 0; //1
+    for (let i = 0; i < numbers.length; i++) { //6
+        sum += numbers[i]; //4
     }
-    return sum;
-}
+    return sum;//1
+} // T_16 (n) = 1 + 6i + 4 + 1  = 1 + 1*n + 1 + 1 = n = 0(n)
