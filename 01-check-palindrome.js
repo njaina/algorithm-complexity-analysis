65f79f0e01f4bbf700cd347793eb47923efa044d
njaina <hei.njaina.2@gmail.com>
2022-03-18 15:42:09 +0300

    calcul de la complexité de T_1

--- 01-check-palindrome.js
+++ 01-check-palindrome.js
@@ -1,2 +1,3 @@
 const checkPalindrome = (inputString) =>
-  inputString === inputString.split("").reverse().join("");
+  inputString === inputString.split("").reverse().join(""); //4
+// T_1 (n)= 4 = 1 = O(1)
